d80aa3ebfa0c10d6e44e8cb08fce84aae82ea7b5
11_cpss <jadenwu1318@gmail.com>
2025-11-05 19:28:04 -0800

    testf

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {getPolls, getPoll, postVote, postPoll} = require("../controllers/pollController")
-//object destructuring, so basically if we did a normal object like postController, it will inherit everything from the require(file name), but by destructuring it, we will save 
-// ACTIVITY: Create routes that call controller functions when rrequested
-router.get("/polls", getPolls);
-router.get("/polls/:id", getPoll);  
-router.post("/vote/:id", postVote);
-router.post("/polls", postPoll);
 
+const pollController = require('../controllers/pollController');
 
+router.get('/polls/id/:id', pollController.getPoll);
+
+router.get('/polls', pollController.getPolls);
+
+router.post('/polls', pollController.postPoll);
+
+router.post('/vote', pollController.postVote);
 
 module.exports = router;
